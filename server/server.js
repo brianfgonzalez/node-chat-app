c605c5728586e59f1f9506ca9a41a44191d5ab59
Brian Gonzalez <brianfgonzalez@gmail.com>
2018-01-29 21:43:20 -0500

    Added styles.css

--- server/server.js
+++ server/server.js
@@ -28,10 +28,10 @@ io.on('connection', (socket) => {
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message)
     io.emit('newMessage', generateMessage(message.from, message.text))
-    callback('This is from the server')
+    callback()
   })
 
-  socket.on('createLocationMessage', (coords) => {
+  socket.on('createLocationMessage', (coords, callback) => {
     io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
   })
 })
